77f8f51aead88c018bcefdf2c0cdd72406a86908
Corno Schraverus <corno@schraverus.com>
2026-05-05 14:54:55 +0200

    .

--- client/src/commands/initialize_authoring_environment_with_this_schema.ts
+++ client/src/commands/initialize_authoring_environment_with_this_schema.ts
@@ -5,9 +5,9 @@ import { $$ as ttt_seal } from 'liana-authoring/dist/implementation/manual/text_
 import * as fs from 'fs'
 import * as path from 'path'
 import * as vscode from 'vscode'
-import * as pareto_unreachable_code_path from 'pareto-core/dist/_p_unreachable_code_path'
 
 import { load_applicable_schema } from '../to_be_backend/load_applicable_schema'
+import create_refinement_context from 'pareto-core/dist/__internals/async/create_refinement_context'
 
 export default function $(): vscode.Disposable {
 	return vscode.commands.registerCommand('liana.initialize_authoring_environment_with_this_schema', async () => {
@@ -16,29 +16,27 @@ export default function $(): vscode.Disposable {
 			vscode.window.showInformationMessage('Open a liana file first to create authoring environment');
 			return;
 		}
+		load_applicable_schema(
+			editor.document,
+			($) => {
 
-		try {
-			load_applicable_schema(
-				editor.document,
-				($) => {
-
-					_p.decide.state($.type, ($) => {
-						switch ($[0]) {
-							case 'read file': return _p.ss($, ($) => {
-								vscode.window.showErrorMessage('Cannot initialize authoring environment because no .liana/schema.slna file could be found in the same directory as the liana file: ' + $.error.message);
-							})
-							case 'parse schema': return _p.ss($, ($) => {
-								vscode.window.showErrorMessage('Cannot initialize authoring environment because the .liana/schema.slna file is not a valid schema.');
-							})
-							default: return _p.au($[0])
-						}
-					})				},
-				async ($) => {
-					const newText = ttt_seal(
+				_p.decide.state($.type, ($) => {
+					switch ($[0]) {
+						case 'read file': return _p.ss($, ($) => {
+							vscode.window.showErrorMessage('Cannot initialize authoring environment because no .liana/schema.slna file could be found in the same directory as the liana file: ' + $.error.message);
+						})
+						case 'parse schema': return _p.ss($, ($) => {
+							vscode.window.showErrorMessage('Cannot initialize authoring environment because the .liana/schema.slna file is not a valid schema.');
+						})
+						default: return _p.au($[0])
+					}
+				})
+			},
+			async ($) => {
+				create_refinement_context<string, string>(
+					(abort) => ttt_seal(
 						editor.document.getText(),
-						(): never => {
-							throw new Error('Cannot initialize authoring environment because the file is not valid Liana.');
-						},
+						($) => abort("Cannot initialize authoring environment because the file is not valid Liana."),
 						{
 							'unmarshall': $,
 							'target': {
@@ -47,48 +45,46 @@ export default function $(): vscode.Disposable {
 							},
 						}
 					)
+				).__extract_data(
+					async ($) => {
+						const newText = $
 
-					const targetUris = await vscode.window.showOpenDialog({
-						canSelectFiles: false,
-						canSelectFolders: true,
-						canSelectMany: false,
-						openLabel: 'Select Directory',
-						title: 'Select directory to save .liana/schema.slna file',
-					});
 
-					if (!targetUris || targetUris.length === 0) {
-						return;
+						const target_URIs = await vscode.window.showOpenDialog({
+							canSelectFiles: false,
+							canSelectFolders: true,
+							canSelectMany: false,
+							openLabel: 'Select Directory',
+							title: 'Select directory to save .liana/schema.slna file',
+						});
+
+						if (!target_URIs || target_URIs.length === 0) {
+							return;
+						}
+
+						const targetPath = target_URIs[0].fsPath;
+						const schemaFilePath = path.join(targetPath, ".liana", "schema.slna");
+
+						const schemaDir = path.dirname(schemaFilePath);
+						fs.mkdirSync(schemaDir, { recursive: true });
+						fs.writeFileSync(schemaFilePath, newText, 'utf8');
+						vscode.window.showInformationMessage(`authoring environment created: ${targetPath}`);
+
+						const openChoice = await vscode.window.showInformationMessage(
+							'Would you like to open the initialized authoring environment?',
+							'Yes', 'No'
+						);
+
+						if (openChoice === 'Yes') {
+							const uri = vscode.Uri.file(targetPath);
+							await vscode.commands.executeCommand('vscode.openFolder', uri, true);
+						}
+					},
+					async ($) => {
+						vscode.window.showErrorMessage(`Cannot create schema: ${$}`);
 					}
-
-					const targetPath = targetUris[0].fsPath;
-					const schemaFilePath = path.join(targetPath, ".liana", "schema.slna");
-
-					const schemaDir = path.dirname(schemaFilePath);
-					fs.mkdirSync(schemaDir, { recursive: true });
-					fs.writeFileSync(schemaFilePath, newText, 'utf8');
-					vscode.window.showInformationMessage(`authoring environment created: ${targetPath}`);
-
-					const openChoice = await vscode.window.showInformationMessage(
-						'Would you like to open the initialized authoring environment?',
-						'Yes', 'No'
-					);
-
-					if (openChoice === 'Yes') {
-						const uri = vscode.Uri.file(targetPath);
-						await vscode.commands.executeCommand('vscode.openFolder', uri, true);
-					}
-				}
-			)
-		} catch (error) {
-			if (error instanceof Error) {
-				console.error('Error generating TypeScript code:', error.message);
-			} else if (error instanceof pareto_unreachable_code_path.Unreachable_Code_Path_Error) {
-				console.error('Unreachable code path reached:', error.message);
-			} else {
-				console.error('Unexpected error:', error);
+				)
 			}
-			const message = error instanceof Error ? error.message : String(error);
-			vscode.window.showErrorMessage(`Cannot create schema: ${message}`);
-		}
+		)
 	})
 }
